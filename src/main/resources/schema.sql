35899ee222857bfc637fa1031be91a850998e204
johaeunn <jheun04@naver.com>
2026-09-21 13:18:31 +0900

    feat: 회원 이름에 unique 제약조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE theme
 CREATE TABLE member
 (
     id       BIGINT              NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255)        NOT NULL,
+    name     VARCHAR(255)        NOT NULL UNIQUE,
     email    VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255)        NOT NULL,
     role     VARCHAR(255)        NOT NULL,
