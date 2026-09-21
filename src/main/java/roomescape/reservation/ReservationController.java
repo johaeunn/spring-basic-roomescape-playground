ab4b2e106180f6f824f8ff2a355b67cb71f690b0
johaeunn <jheun04@naver.com>
2026-09-21 17:00:12 +0900

    refactor: 예약 요청 검증 및 예외 처리 분리

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -1,5 +1,6 @@
 package roomescape.reservation;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,13 +28,7 @@ public class ReservationController {
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity create(@RequestBody ReservationRequest reservationRequest, LoginMember loginMember) {
-        if (reservationRequest.getDate() == null
-                || reservationRequest.getTheme() == null
-                || reservationRequest.getTime() == null) {
-            return ResponseEntity.badRequest().build();
-        }
-
+    public ResponseEntity create(@Valid @RequestBody ReservationRequest reservationRequest, LoginMember loginMember) {
         ReservationResponse reservation = reservationService.save(reservationRequest, loginMember);
 
         return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).body(reservation);
